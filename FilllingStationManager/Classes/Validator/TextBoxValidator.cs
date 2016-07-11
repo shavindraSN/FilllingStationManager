a241823226bf978bfc8e9efa6eb86b8133cdf50d
shavindraSN <shavindramms1@gmail.com>
2016-07-11 21:39:47 +0530

    Added standard comments to TextBoxValidator class methods

--- FilllingStationManager/Classes/Validator/TextBoxValidator.cs
+++ FilllingStationManager/Classes/Validator/TextBoxValidator.cs
@@ -15,6 +15,13 @@ namespace FillingStationManager.Classes.Validator
 
         StringValidator sv = new StringValidator();
 
+        /// <summary>
+        /// Check all the given TextBoxes are empty. 
+        /// If any TextBox in the given set is empty, method returns true. 
+        /// Otherwise returns false
+        /// </summary>
+        /// <param name="textBoxes"></param>
+        /// <returns>Returns true or false</returns>
         public Boolean isEmpty(TextBox[] textBoxes)
         {
             foreach (TextBox textBox in textBoxes)
@@ -27,6 +34,12 @@ namespace FillingStationManager.Classes.Validator
             return true;
         }
 
+        /// <summary>
+        /// Check all the given TextBoxes are contains only numbers.
+        /// If all the textboxes contains numbers returns true else return false
+        /// </summary>
+        /// <param name="textBoxes"></param>
+        /// <returns>Returns true or false.</returns>
         public Boolean isNumber(TextBox[] textBoxes)
         {
             foreach (TextBox textBox in textBoxes)
