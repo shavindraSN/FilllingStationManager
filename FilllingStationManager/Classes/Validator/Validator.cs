af8c56e1e03b53485364d6234c00aa8d8a0c53a8
Manathunga, Shavindra <Shavindra.Manathunga@pearson.com>
2016-07-17 08:05:41 +0530

    String validation methods, document style comments added

--- FilllingStationManager/Classes/Validator/Validator.cs
+++ FilllingStationManager/Classes/Validator/Validator.cs
@@ -8,6 +8,11 @@ namespace FillingStationManager.Classes.Validator
 {
     class Validator
     {
+        /// <summary>
+        /// Checks for numbers for given string. Number can be int or double
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>Returns true or false</returns>
         public Boolean isNumber(String value)
         {
             int tempi;
@@ -23,6 +28,11 @@ namespace FillingStationManager.Classes.Validator
             }
         }
 
+        /// <summary>
+        /// Check whether given string is a Integer(Checks it can convert to integer)
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>Returns true or false</returns>
         public Boolean isInt(String value)
         {
             int tempi;
@@ -36,6 +46,11 @@ namespace FillingStationManager.Classes.Validator
             }
         }
 
+        /// <summary>
+        /// Check whether given string is empty(null or empty)
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>Returns true or false</returns>
         public Boolean isEmpty(String value)
         {
             if (value == "" || value == null)
@@ -46,6 +61,12 @@ namespace FillingStationManager.Classes.Validator
             {
                 return false;
             }
+        }
+
+        public Boolean isEmail(String value)
+        {
+            // Add the code to implement emails validation
+            return false;
         }
     }
 }
